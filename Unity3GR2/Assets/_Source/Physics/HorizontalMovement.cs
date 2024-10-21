e93694af1fa6a98e77f6ad819443fcbfc234e5e4
MeylishDLC <meylishy20@gmail.com>
2024-10-21 23:47:27 +0300

    feat: added horizontal movement based on motor

---
+++ Unity3GR2/Assets/_Source/Physics/HorizontalMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Physics
+{
+    public class HorizontalMovement: MonoBehaviour
+    {
+        [SerializeField] private float speed = 100f;
+
+        private HingeJoint hingeJoint;
+        private JointMotor motor;
+
+        private void Start()
+        {
+            hingeJoint = GetComponent<HingeJoint>();
+            motor = hingeJoint.motor;
+            motor.force = 1000;
+        }
+
+        private void Update()
+        {
+            if (hingeJoint.angle >= hingeJoint.limits.max)
+            {
+                motor.targetVelocity = -speed;
+            }
+            else if (hingeJoint.angle <= hingeJoint.limits.min)
+            {
+                motor.targetVelocity = speed;
+            }
+
+            hingeJoint.motor = motor;
+        }
+    }
+}
